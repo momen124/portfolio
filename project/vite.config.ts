0ce43d6a6838e07d2124dd6f4b0f8f425a02faee
momen124 <momenhossny2004@gmail.com>
2025-01-21 23:24:52 +0200

    Configure GitHub Pages deployment

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,8 +1,10 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  // Replace with your actual repo name
+  base: '/vite-react-typescript-starter/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
